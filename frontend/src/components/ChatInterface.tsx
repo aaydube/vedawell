c08f25c30018300832e8f4f00a9b5ebc83b15e20
Aayush <dubeyaayush019@gmail.com>
2025-05-02 10:20:47 +0530

    init

--- frontend/src/components/ChatInterface.tsx
+++ frontend/src/components/ChatInterface.tsx
@@ -4,7 +4,7 @@ import ChatInput from './ChatInput';
 
 const ChatInterface: React.FC = () => {
   return (
-    <div className="flex h-screen bg-gray-50">
+    <div className="flex min-h-[100dvh] bg-gray-50">
       <main className="flex-1 flex flex-col">
         <header className="h-14 border-b border-amber-200 flex items-center px-4 bg-white">
           <h1 className="text-lg pl-4 font-medium">VedaWell</h1>
